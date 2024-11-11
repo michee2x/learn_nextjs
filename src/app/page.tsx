38e24d6f78efb47504b13a0080d2646af0c6ea95
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 06:08:55 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,11 +33,11 @@ return (
 const Editor = ({title, setTitle, body, setBody}:any) => {
 return (
 <>
-<input value={title} onChange={(e:any) => setTitle(e.target.value)} type="text" classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="note title pls..." required />
+<input value={title} onChange={(e:any) => setTitle(e.target.value)} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="note title pls..." required />
   
 
-<label value={body} onChange={(e:any) => setBody(e.target.value)} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your note</label>
-<textarea rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts 🤔 here..."></textarea>
+<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your note</label>
+<textarea value={body} onChange={(e:any) => setBody(e.target.value)} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts 🤔 here..."></textarea>
 
 </>
 )

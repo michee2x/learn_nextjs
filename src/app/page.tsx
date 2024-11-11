e6df6e0c7dacd398ad33a23a40ccce46f17a9d4c
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 01:22:05 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,14 +35,15 @@ export default function Home() {
  dark:bg-gray-900">
   <div class="flex flex-col relative items-center justify-center px-6 py-8 h-full w-full lg:py-0">
 
-<div>
-<p>Notes</p>
+<div className="w-full bg-gray-900 text-gray-100 flex flex-col gap-8 p-6 min-h-screen">
+<p className="text-2xl">Notes</p>
+<SearchInput />
 
 
 
 </div>
       
-      <div class="w-full h-14 flex fixed buttom-0 justify-between items-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+      <div className="w-full h-14 flex fixed buttom-0 justify-between items-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
 <span className="text-2xl"><GrNotes /></span>
 <span className="text-2xl"><RiTodoFill /></span>
 

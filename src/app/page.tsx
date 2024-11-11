e220fdeb47f12575c30931568b400ec96e9fead0
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 02:00:27 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,11 +36,13 @@ export default function Home() {
 
 
 </div>
-</div>
-<div className="w-full h-14 flex fixed bottom-0 justify-between items-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+
+
+<div className="w-full h-14 flex fixed  bottom-5 justify-between items-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
 <span className="text-2xl"><GrNotes /></span>
 <span className="text-2xl"><RiTodoFill /></span>
 
+</div>
 </div>
 </main>
   )

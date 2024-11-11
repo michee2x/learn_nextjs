1b4d8dc186d4ebd9d379621e314e6087279d7f65
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 05:56:08 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import {RiTodoFill} from 'react-icons/ri'
 import {FaArrowLeft} from 'react-icons/fa'
 import {MdOutlineShare} from 'react-icons/md'
 import {CiMenuKebab} from 'react-icons/ci'
+import {GrStatusGood} from "react-icons/gr"
 
 const SearchInput = ({notes}: any) => {
 
@@ -29,13 +30,13 @@ return (
 )
 }
 
-const Editor = () => {
+const Editor = ({title, setTitle, body, setBody}:any) => {
 return (
 <>
-<input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="note title pls..." required />
+<input value={title} onChange={(e:any) => setTitle(e.target.value)} type="text" classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="note title pls..." required />
   
 
-<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your note</label>
+<label value={body} onChange={(e:any) => setBody(e.target.value)} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your note</label>
 <textarea rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts 🤔 here..."></textarea>
 
 </>
@@ -94,6 +95,8 @@ const [note, setNote] = useState(true)
 const [singleNote, setSingleNote] = useState({text:"", time:""})
 const [show, setShow] = useState(false)
 const [edit, setEdit] = useState(false)
+const [title, setTitle] = useState("")
+const [body, setBody] = useState("")
 
   return (
 <>
@@ -157,13 +160,13 @@ const [edit, setEdit] = useState(false)
 <span>Edit</span>
 </p>
 
-<p className="w-24 h-full flex items-center justify-between">
-<MdOutlineShare />
+<p className={`w-24 ${(body && title) ? "block" : "hidden"} h-full flex items-center justify-end`} onClick={() => setEdit(false)>
+<GrStatusGood />
 </p>
 </div>
 
 <div className="w-full relative h-full bg-gray-900 px-10 px-4">
-<Editor />
+<Editor title={title} setTitle={setTitle} body={body} setBody={setBody} />
 </div>
 
  </main>

55917177421c724a968c8aa727c5eb5aac7ee485
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 02:41:19 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ return (
 const Todos = ({todoData} : any) => {
 return (
 <>
-{ noteData.map((e: string) => {
+{ todoData.map((e: string) => {
 return (
 <div key={e} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 <p className='text-xl text-white'>{e}</p>

fe7f6dbcaccf5f711f91f835788f7561edbb25aa
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 05:29:14 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,17 @@ return (
 )
 }
 
+const Editor = () => {
+return (
+<>
+
+<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your note</label>
+<textarea rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+</>
+)
+}
+
 
 const Notes = ({noteData, setSingleNote} : any) => {
 return (

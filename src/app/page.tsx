ec1b2e6967f972da72b98087b9099b991c2ecaee
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 18:39:01 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,11 +32,15 @@ return (
     <div className="relative w-full">
         <input value={search} onChange={(e) => setSearch(e.target.value?.toLowerCase())} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={`Search ${notes ? "notes" : "to-do"}...`} />
     </div>
-    <button type="submit" onClick={() => SearchIterate(notes, setFiltered, search, noteData, todoData)} className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <svg className={`w-4 ${!filtered.length ? "block" : "hidden"} h-4`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <button type="submit" onClick={() => SearchIterate(notes, setFiltered, search, noteData, todoData)} className={`${!filtered.length ? "block" : "hidden"} p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>
+        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
         </svg>
-<span onClick={() => setFiltered([])} className={`w-4 ${filtered.length ? "block" : "hidden"} search h-4 flex items-center justify-center text-xl`}><TbZoomCancel /></span>
+        <span className="sr-only">Search</span>
+    </button>
+
+<button type="submit" onClick={() => setFiltered([])} className={`${filtered.length ? "block" : "hidden"}  p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>
+<span className="w-4 search h-4 flex items-center justify-center text-xl"}><TbZoomCancel /></span>
         <span className="sr-only">Search</span>
     </button>
 </div>

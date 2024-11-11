34e1aefb0cb2552e55afd4194ebbdcdb950e431b
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 02:59:36 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ return (
 <form className="flex items-center max-w-sm mx-auto">   
     <label className="sr-only">Search {notes ? "notes" : "to-do"}</label>
     <div className="relative w-full">
-        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Notes..." required />
+        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=`Search ${notes ? "notes" : "to-do"}...` />
     </div>
     <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -34,7 +34,7 @@ return (
 { noteData.map((e: string) => {
 return (
 <div key={e} className="bg-gray-50 flex flex-col gap-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-<p className='text-xl text-white'>{e}</p>
+<p className='text-xl text-white'>{e.slice(0,17)}</p>
 <p className='text-sm text-gray-300'>8:00pm, November 8, 2024 </p>
 
 </div>
@@ -50,8 +50,9 @@ return (
 <>
 { todoData.map((e: string) => {
 return (
-<div key={e} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-<p className='text-xl text-white'>{e}</p>
+<div key={e} className="bg-gray-50 flex gap-3 items-center justify-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <input type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
+                    <label className="ml-3 min-w-0 flex-1 text-gray-500">{e.slice(0, 16)}</label>
 </div>
 )})
 }

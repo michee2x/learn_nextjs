473cfe05b0591d5b1ca01233a9c719f8d1819932
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 04:33:50 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ return (
 const Notes = ({noteData, setSingleNote} : any) => {
 return (
 <>
-{ noteData.map((e: string) => {
+{ noteData.map((e: any) => {
 return (
 <div key={e} onClick={() => setSingleNote(e)} className="bg-gray-50 flex flex-col gap-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full min-h-16 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 <p className='text-lg text-white'>{e.text.slice(0,30)}...</p>

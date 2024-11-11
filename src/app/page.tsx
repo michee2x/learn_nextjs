0edb52a6b0ebbb8cff23655f2d29a98328ce0b93
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 02:34:51 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ return (
 <>
 { noteData.map(e => {
 return (
-<div className="bg-gray-50 flex flex-col gap-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<div key={e} className="bg-gray-50 flex flex-col gap-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 <p className='text-xl text-white'>{e}</p>
 <p className='text-sm text-gray-300'>8:00pm, November 8, 2024 </p>
 
@@ -49,7 +49,7 @@ return (
 <>
 { noteData.map(e => {
 return (
-<div className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<div key={e} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 <p className='text-xl text-white'>{e}</p>
 </div>
 )})

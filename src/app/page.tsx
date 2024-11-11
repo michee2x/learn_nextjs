3fc191475a4e58b7441eba0dcb833315218022b9
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 01:42:49 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,6 @@ return (
 <form className="flex items-center max-w-sm mx-auto">   
     <label className="sr-only">Search</label>
     <div className="relative w-full">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg className="w-4 h-4 hidden text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
-            </svg>
-        </div>
         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Notes..." required />
     </div>
     <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -32,7 +27,7 @@ return (
 export default function Home() {
   return (
     <main className="bg-gray-50 h-screen w-screen bg-gray-900">
-  <div className="flex flex-col relative items-center justify-center px-6 py-8 h-full w-full lg:py-0">
+  <div className="flex flex-col items-center justify-center px-6 py-8 h-full w-full lg:py-0">
 
 <div className="w-full bg-gray-900 text-gray-100 flex flex-col gap-8 p-6 min-h-screen">
 <p className="text-2xl">Notes</p>

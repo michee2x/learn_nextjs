2bad7640fe219be2b79a7f5c7835bda686648b4f
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-11 02:39:48 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const SearchInput = ({notes}: any) => {
 return (
 
 <form className="flex items-center max-w-sm mx-auto">   
-    <label className="sr-only">Search {note ? "notes" : "to-do"}</label>
+    <label className="sr-only">Search {notes ? "notes" : "to-do"}</label>
     <div className="relative w-full">
         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Notes..." required />
     </div>

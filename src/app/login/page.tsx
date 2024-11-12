20ae7b3df9e15283081a48148bff9dbccc46b08d
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-11-12 13:11:33 +0100

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,44 +48,37 @@ export default function Login() {
 
   return (
     <div className="flex flex-col gap-2 items-center justify-center max-w-lg mx-auto min-h-screen py-2">
-      <h1 className="text-2xl ">Login</h1>
-      <hr />
-
-      <div className="flex flex-col gap-2 w-full">
-        <label htmlFor="email">email</label>
-        <input
-          className="p-2 px-4 text-gray-950 border border-gray-300 rounded-lg my-2 focus:outline-none focus:border-gray-600"
-          id="email"
-          type="email"
-          inputMode="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="email"
-        />
+    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                          <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                          </div>
+                          <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                          </div>
+                      </div>
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
       </div>
-      <div className="flex flex-col gap-2 w-full">
-        <label htmlFor="password">password</label>
-        <input
-          className="p-2 px-4 text-gray-950 border border-gray-300 rounded-lg my-2 focus:outline-none focus:border-gray-600"
-          id="password"
-          type="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="password"
-        />
-      </div>
-
-      <button
-        onClick={onLogin}
-        className={`
-        ${buttonDisabled ? "pointer-events-none opacity-50" : ""} 
-        ${loading ? "italic" : ""}
-        ${"w-full p-2 border border-gray-300 rounded-lg my-2 focus:outline-none focus:border-gray-600"}`}
-      >
-        {loading ? "Loading..." : "Login"}
-      </button>
-
-      <Link href="/signup">Visit sign up page</Link>
-    </div>
   );
 }
